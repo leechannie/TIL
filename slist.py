00b653ebda885a093235a3bea5d6233425dbf293
leechannie <ioio000213@gmail.com>
2020-08-25 12:39:37 +0900

    return

---
+++ slist.py
@@ -0,0 +1,16 @@
+class slist:
+    class Node:
+        def __init__(self,item,link):
+                self.item = item
+                self.next = link
+    
+    def __init__(self):
+        self.head = None   
+        self.size = 0
+    
+    def size(self)
+        retrun self.size
+    
+    def is_empty(self)
+        return self.size == 0
+        
