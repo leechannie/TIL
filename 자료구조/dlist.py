4391ecd65eebafb83f5f870e26c9bac571e1d1be
leechannie <ioio000213@gmail.com>
2020-08-28 18:21:48 +0900

    더블리링크드리스트 초기화

---
+++ 자료구조/dlist.py
@@ -0,0 +1,18 @@
+class dlist:
+       class Node:
+           def __init__(self,item,next,prev):
+                self.item = item
+                self.next = next
+                self.prev = prev
+
+        def __init__ (self):
+            self.head = self.Node(None, None, None)
+            self.tail = self.Node(None,self.head,None)
+            self.head.next = self.tail
+            sefl.size = 0
+
+        def size(self):
+            return self.size
+            
+        def is_emtpy(self):
+            return self.size == 0
