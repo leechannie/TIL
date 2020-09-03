7b6a398b491865ac6cb31b48b602deb2d627c99f
leechannie <ioio000213@gmail.com>
2020-09-03 16:28:17 +0900

    백준알고리즘 5543

---
+++ Mcdonald.py
@@ -0,0 +1,21 @@
+def low(item):
+    if item > 2000 or item < 100:
+        print("가격은 100원 이상 2000원 이하 입니다.")
+        item = low(int(input("상덕버거의 가격을 입력해주세요: ")))
+    else:
+        item = item
+    return item
+
+def b_compare(s,j,h): 
+    return min(s,j,h)
+
+def d_compare(c,cd):
+    return min(c,cd)
+
+s = low(int(input()))
+j = low(int(input()))
+h = low(int(input()))
+c = low(int(input()))
+cd = low(int(input()))
+
+print(b_compare(s,j,h)+d_compare(c,cd)-50)
