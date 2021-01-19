0c976c387d6a2b332673db3bb8cc4595aad09d9c
leechannie <61904561+leechannie@users.noreply.github.com>
2021-01-19 20:37:46 +0900

    간단한 주문 프로그램
    https://sandbox.cs50.io/

--- CS50/shop.c
+++ CS50/shop.c
@@ -3,14 +3,15 @@
 
 int main (void){
     int water_m = 5;
-    int answer = get_int("수박 주문 개수: ");
-    if (water_m > answer){
+    int num = get_int("수박 주문 개수: ");
+    if (water_m >= num){
         printf("***** 수박 주문 *****\n");
-        printf("주문건수: %i 건\n", answer);
+        printf("주문건수: %i 건\n", num);
         printf("기존 재고량: 5개\n");
-        printf("남은 재고량: %i 개\n", water_m - answer);
-        printf("매출액(부가세포함): %.0f 원\n", (answer*10000)+(answer*10000*0.1));
+        printf("남은 재고량: %i 개\n", water_m - num);
+        printf("매출액(부가세포함): %.0f 원\n", num*10000*1.1);
+    }
+    else {
+        printf("재고가 부족합니다.");
     }
 }
-
-
