bfa8792d4643d2ce37917303779eca0eec54d6e0
leechannie <61904561+leechannie@users.noreply.github.com>
2021-09-04 13:14:26 +0900

    모바일 프로그래밍

---
+++ Android/leeCH_60191680_FINAL/app/src/main/java/com/example/leech_60191680_final/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.leech_60191680_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
