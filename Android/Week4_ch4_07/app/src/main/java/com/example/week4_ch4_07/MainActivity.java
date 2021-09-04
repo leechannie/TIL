8a08bbb2f8f3336750be472c498064ee60fa7059
leechannie <61904561+leechannie@users.noreply.github.com>
2021-09-04 13:29:36 +0900

    모바일 프로그래밍

---
+++ Android/Week4_ch4_07/app/src/main/java/com/example/week4_ch4_07/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.week4_ch4_07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox checkBox, checkBox2, checkBox3;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        button = (Button) findViewById(R.id.button);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                button.setEnabled(true);
+            }
+        });
+
+        checkBox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                button.setClickable(true);
+            }
+        });
+
+        checkBox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                button.setRotation(45);
+            }
+        });
+    }
+}
---
+++ Android/Week5_ch5lay/app/src/main/java/com/example/week5_ch5lay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.week5_ch5lay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
