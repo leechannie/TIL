308d1463ac2df175ef0d6a5e4c12b9338b410622
leechannie <61904561+leechannie@users.noreply.github.com>
2022-10-02 12:29:48 +0900

    Add files via upload

---
+++ Game Programming/과제1-1/Assets/RouletteController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;  // 회전 속도  
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // 마우스 왼쪽 버튼을 누르고 있는 도중의 처리   
+        if(Input.GetMouseButton(0))
+        {
+            this.rotSpeed = 20;
+        }
+
+        // 회전 속도만큼 룰렛을 회전시킨다
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛을 감속시킨다(추가)
+        this.rotSpeed *= 0.96f;
+
+
+    }
+}
